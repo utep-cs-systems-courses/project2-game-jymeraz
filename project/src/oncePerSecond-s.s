028babbcb5454876d3082992b7fee2e10c01352a
student <janethmeraz5@gmail.com>
2021-11-02 14:15:10 -0600

    fixed assembly code

--- project/src/oncePerSecond-s.s
+++ project/src/oncePerSecond-s.s
@@ -2,27 +2,29 @@
 	.p2align 1,0
 	.text
 	
-	.extern blinkLimit, color, direction
+	.extern blinkLimit
+	.extern color
+	.extern direction
 	.global oncePerSecond
+	
 oncePerSecond:
 	cmp #8, &blinkLimit
 	jnc first
 	mov #0, &direction
 	xor #1, &color
-	jmp first
 first:
-	cmp &blinkLimit, #0
+	mov #0, r12
+	cmp &blinkLimit, r12
 	jnc second
 	mov #1, &direction
-	jmp second
 second:
 	cmp #1, &direction
 	jne third
 	add #1, &blinkLimit
 	jmp out
 third:
-	sub #1, &blinkLimit
-	jmp out			
+	sub #1, &blinkLimit	
 out:
 	pop r0			
+	
 	
